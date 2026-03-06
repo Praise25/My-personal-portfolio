60a43b70cffcb85ab8753d06e86c2f97d65c00a7
Anene Praise <anenepraise1@gmail.com>
2026-03-06 09:59:17 +0100

    feat: implement white oval designs in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,37 @@
 import heroImg from "../assets/hero-picture.png";
+import whiteCircle from "../assets/white-circle.svg";
+import ovalsDesign from "../assets/ovals.svg"
 
 import classes from "./Hero.module.css";
 
 export default function Hero() {
   return (
-    <div className={classes["hero-section"]}>
-      <div>
-        <h1>
-          Nice to meet you! {"\n"}
-          I'm
-          <span>
-            Adam Keyes.
-            <hr />
-          </span>
-        </h1>
-
-        <p>
-          Based in the UK, I’m a front-end developer {"\n"}
-          passionate about building accessible web apps {"\n"}
-          that users love.
-        </p>
-        <button>CONTACT ME</button>
+    <>
+      <div className={classes["hero-section"]}>
+        <div>
+          <h1>
+            Nice to meet you! {"\n"}
+            I'm
+            <span>
+              Adam Keyes.
+              <hr />
+            </span>
+          </h1>
+      
+          <p>
+            Based in the UK, I’m a front-end developer {"\n"}
+            passionate about building accessible web apps {"\n"}
+            that users love.
+          </p>
+          <button>CONTACT ME</button>
+        </div>
+      
+        <div className={classes["image-container"]}>
+          <img src={heroImg} alt="Adam Keyes" />
+          <img className={classes["white-circle"]} src={whiteCircle} alt="White Circle" />
+        </div>
       </div>
-
-      <div className={classes["image-container"]}>
-        <img src={heroImg} alt="Adam Keyes" />
-      </div>
-    </div>
+      <img className={classes["ovals-design"]} src={ovalsDesign} alt="Ovals Design" />
+    </>
   );
 }
