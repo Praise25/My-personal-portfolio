2ae135e278cde556adc3a4e787c5af13b6ec596f
Anene Praise <anenepraise1@gmail.com>
2026-03-10 12:29:52 +0100

    feat: implement form validation and submission

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,35 +1,80 @@
 import Navbar from "./UI/Navbar";
-import ovalsdesign5 from "../assets/ovals-5.svg"
+import ovalsdesign5 from "../assets/ovals-5.svg";
 import Button from "./UI/Button";
+import z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import classes from "./ContactMe.module.css";
 
+const schema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email("Invalid email address"),
+  message: z.string().min(1, "Message is required"),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function ContactMe() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <footer className={classes["root-container"]}>
       <div className={classes["main-content"]}>
         <article>
           <h1>Contact</h1>
           <p>
-            I would love to hear about your project and how I {"\n"} could
-            help. Please fill in the form, and I’ll get back {"\n"}
+            I would love to hear about your project and how I {"\n"} could help.
+            Please fill in the form, and I’ll get back {"\n"}
             to you as soon as possible.
           </p>
         </article>
-        <form action="">
-          <input type="text" id="name" placeholder="NAME" />
+        
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("name")}
+            placeholder="NAME"
+            className={errors?.name ? classes.error : ""}
+          />
+          <p className={classes["error-msg"]}>
+            {errors?.name && errors.name.message}
+          </p>
 
-          <input type="email" id="email" placeholder="EMAIL" />
+          <input
+            {...register("email")}
+            placeholder="EMAIL"
+            className={errors?.email ? classes.error : ""}
+          />
+          <p className={classes["error-msg"]}>
+            {errors?.email && errors.email.message}
+          </p>
 
           <textarea
-            name="message"
-            id="message"
+            {...register("message")}
             placeholder="MESSAGE"
+            className={errors?.message ? classes.error : ""}
           ></textarea>
+          <p className={classes["error-msg"]}>
+            {errors?.message && errors.message.message}
+          </p>
+
           <Button className={classes.button}>SEND MESSAGE</Button>
         </form>
       </div>
-      <img className={classes["oval-design"]} src={ovalsdesign5} alt="Oval Design 5" />
+      <img
+        className={classes["oval-design"]}
+        src={ovalsdesign5}
+        alt="Oval Design 5"
+      />
       <div className={classes["navbar-container"]}>
         <Navbar className={classes.navbar} brandName="adamkeyes" />
       </div>
