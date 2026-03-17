17abe16137c1520dc87e3ddbc7847b012e868fa6
Anene Praise <anenepraise1@gmail.com>
2026-03-17 12:56:07 +0100

    feat: revamp skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,20 +1,22 @@
 import classes from "./Skills.module.css";
-import ovalsDesign6 from "../assets/ovals-6.svg"
+import ovalsDesign6 from "../assets/ovals-6.svg";
 
 import { SKILLS } from "../data/skills";
 
 export default function Skills() {
   return (
     <div className={classes["root-container"]}>
+      <h2>Tools of the Trade</h2>
       <ul className={classes["list-container"]}>
         {SKILLS.map((skill) => (
           <li key={skill.id}>
-            <h2>{skill.name}</h2>
-            <p>{skill.years} Years Experience</p>
+            {/* <h2>{skill.name}</h2> */}
+            <img src={skill.logo} alt={`${skill.name} logo`} />
+            <p>{skill.name}</p>
           </li>
         ))}
       </ul>
-      <img src={ovalsDesign6} alt="Oval Design 2" />
+      <img className={classes["ovals-design"]} src={ovalsDesign6} alt="Oval Design 2" />
     </div>
   );
 }
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,14 +1,21 @@
+import reactLogo from "../assets/react.svg";
+import typescriptLogo from "../assets/typescript.svg";
+import nextJsLogo from "../assets/nextjs.svg";
+import nodeJsLogo from "../assets/nodejs.svg";
+import pythonLogo from "../assets/python.svg";
+import tailwindLogo from "../assets/tailwindcss.svg";
+
 interface Skill {
   id: number;
   name: string;
-  years: number;
+  logo: string;
 }
 
 export const SKILLS: Skill[] = [
-  { id: 0, name: "React", years: 4 },
-  { id: 1, name: "Typescript", years: 4 },
-  { id: 2, name: "Express", years: 4 },
-  { id: 3, name: "NextJs", years: 4 },
-  { id: 4, name: "Python", years: 3 },
-  { id: 5, name: "Tailwind", years: 3 },
+  { id: 0, name: "React", logo: reactLogo },
+  { id: 1, name: "Typescript", logo: typescriptLogo },
+  { id: 2, name: "NextJs", logo: nextJsLogo },
+  { id: 3, name: "NodeJs", logo: nodeJsLogo },
+  { id: 4, name: "Python", logo: pythonLogo },
+  { id: 5, name: "Tailwind", logo: tailwindLogo },
 ];
